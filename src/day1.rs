1281b0effed231982402366e56ae6e2a7d602dce
Dominik Baumann <hey@sobrinth.com>
2024-12-01 20:49:38 +0100

    Scaffold for running code

---
+++ src/day1.rs
@@ -0,0 +1,9 @@
+﻿#[aoc_generator(day1)]
+pub fn generator(_input: &str) -> (Vec<u32>, Vec<u32>) {
+    todo!()
+}
+
+#[aoc(day1, part1)]
+pub fn part_1(_input: &(Vec<u32>, Vec<u32>)) -> u32 {
+    todo!()
+}
