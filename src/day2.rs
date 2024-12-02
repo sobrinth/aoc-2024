6692c173dbf188dc40c8951f93bad0a9d2915c0d
Dominik Baumann <hey@sobrinth.com>
2024-12-02 08:58:05 +0100

    day02 -> part02

--- src/day2.rs
+++ src/day2.rs
@@ -1,19 +1,42 @@
 use itertools::Itertools;
 
-pub fn part_1(input: &str) -> i32 {
+pub fn gen(input: &str) -> Vec<Vec<i32>> {
     input
         .lines()
         .map(|l| {
             l.split_whitespace()
                 .map(|x| x.parse::<i32>().unwrap())
                 .collect::<Vec<i32>>()
-        })
+        }).collect::<Vec<Vec<i32>>>()
+}
+
+pub fn part_1(input: &Vec<Vec<i32>>) -> i32 {
+    input
+        .iter()
         .map(|r| {
             let mut ok = false;
             ok |= r.iter().tuple_windows().all(|(a, b)| a < b);
             ok |= r.iter().tuple_windows().all(|(a, b)| a > b);
-            let low_diff= r.iter().tuple_windows().all(|(a, b)| (a - b).abs() <= 3);
+            let low_diff = r.iter().tuple_windows().all(|(a, b)| (a - b).abs() <= 3);
             ok && low_diff
+        })
+        .filter(|x| *x)
+        .count() as i32
+}
+
+pub fn part_2(input: &Vec<Vec<i32>>) -> i32 {
+    input
+        .iter()
+        .map(|r| {
+            (0..r.len()).any(|i| {
+                let mut rem = r.to_vec();
+                rem.remove(i);
+                let mut ok = false;
+                ok |= rem.iter().tuple_windows().all(|(a, b)| a < b);
+                ok |= rem.iter().tuple_windows().all(|(a, b)| a > b);
+                let low_diff = rem.iter().tuple_windows().all(|(a, b)| (a - b).abs() <= 3);
+                ok && low_diff
+            })
         })
         .filter(|x| *x)
         .count() as i32
@@ -32,5 +55,16 @@ mod tests {
         8 6 4 4 1
         1 3 6 7 9";
         assert_eq!(part_1(s), 2);
+    }
+
+    #[test]
+    fn part2() {
+        let s = "7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9";
+        assert_eq!(part_2(s), 4);
     }
 }
