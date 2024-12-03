134532064322dec62b36f21f31c243af55a00aaf
Dominik Baumann <hey@sobrinth.com>
2024-12-03 08:05:28 +0100

    day03 -> part02: change regex

--- src/day3.rs
+++ src/day3.rs
@@ -1,34 +1,41 @@
 use regex::Regex;
 
 pub fn part_1(input: &str) -> i32 {
-    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let regex = Regex::new(r"mul\((?<x>\d+),(?<y>\d+)\)").unwrap();
 
-    regex.captures_iter(input)
-        .map(|caps| {
-            (caps[1].parse::<i32>().unwrap()) * (caps[2].parse::<i32>().unwrap())
-        })
+    regex
+        .captures_iter(input)
+        .map(|caps| (caps["x"].parse::<i32>().unwrap()) * (caps["y"].parse::<i32>().unwrap()))
         .sum()
 }
 
 pub fn part_2(input: &str) -> i32 {
     let mut enable = true;
-    let regex = Regex::new(r"mul\((\d+),(\d+)\)|don't\(\)|do\(\)").unwrap();
-    
-    regex.captures_iter(input)
-        .filter_map(|caps| {
-            if let (Some(x), Some(y)) = (caps.get(1), caps.get(2)) {
+    let regex = Regex::new(r"(?<cmd>(mul|don't|do))\(((?<x>\d+),(?<y>\d+))?\)").unwrap();
+
+    regex
+        .captures_iter(input)
+        .filter_map(|caps| match &caps["cmd"] {
+            "mul" => {
                 if enable {
-                    Some((x.as_str().parse::<i32>().unwrap()) * (y.as_str().parse::<i32>().unwrap()))
+                    if let (Some(x), Some(y)) = (caps.name("x"), caps.name("y")) {
+                        Some(x.as_str().parse::<i32>().unwrap() * y.as_str().parse::<i32>().unwrap())
+                    } else {
+                        None
+                    }
+                } else {
+                    None
                 }
-                else { None }
-            } else { 
-                match &caps[0] { 
-                    "don't()" => enable = false,
-                    "do()" => enable = true,
-                    _=> {}
-                }
+            }
+            "don't" => {
+                enable = false;
                 None
             }
+            "do" => {
+                enable = true;
+                None
+            }
+            _ => None,
         })
         .sum()
 }
@@ -42,7 +49,7 @@ mod tests {
         let s = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!(part_1(s), 161);
     }
-    
+
     #[test]
     fn part2() {
         let s = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
